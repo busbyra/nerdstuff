58067d76d20ee91bd7793cff1e07de059e175ea9
Ryan Busby <ryanbusbyalt@gmail.com>
2017-08-03 18:55:02 +0000

    Moving ccc stuff to its own file

---
+++ ccc.py
@@ -0,0 +1,14 @@
+cultures =['Borca', 'Franka', 'Pollen', 'Balkhan', 'Hyprispania', 'Purgare', 
+'Africa']
+
+concepts = ['Adventurer', 'Creator', 'Mentor', 'Martyr', 'Ruler', 'Seeker', 
+'Healer', 'Traditionalist', 'Mediator', 'Hermit', 'Heretic', 'Conqueror', 
+'Abomination', 'Destroyer', 'Chosen', 'Defiler', 'Protector', 'Visionary', 
+'Zealot', 'Disciple', 'Righteous', 'Traveler']
+
+cults = ['Spitalians', 'Chroniclers', 'Hellvetics', 'Judges', 'Clanners', 
+'Scrappers', 'Neoubyans', 'Scourgers', 'Anubians', 'Jehammedans', 
+'Apocalyptics', 'Anabaptists', 'Palers']
+
+for i in cults:
+    print i
